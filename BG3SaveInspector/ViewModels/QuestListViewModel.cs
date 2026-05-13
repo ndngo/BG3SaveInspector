33ca30b5da95fcd9748f1945531bd5579dde97a4
ndngo <ngonathanieldean@gmail.com>
2026-05-13 00:08:25 -0400

    populate listview with flags

--- BG3SaveInspector/ViewModels/QuestListViewModel.cs
+++ BG3SaveInspector/ViewModels/QuestListViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Navigation;
 using LSLib.LS;
 
 namespace BG3SaveInspector.ViewModels
@@ -19,7 +20,56 @@ namespace BG3SaveInspector.ViewModels
 
         public void Populate(Resource resource)
         {
+            Quests.Clear();
 
+            System.Diagnostics.Debug.WriteLine("---POPULATE()---");
+
+            if (!resource.Regions.ContainsKey("Journal"))
+            {
+                return;
+            }
+            
+            var journal = resource.Regions["Journal"];
+            
+            if (!journal.Children.ContainsKey("Quests"))
+            {
+                return;
+            }
+
+            var questsNode = journal.Children["Quests"].First();
+
+            System.Diagnostics.Debug.WriteLine($"Reading in quests...");
+            var progressNodes = journal.Children["Quests"][0].Children["Quests"][0].Children["QuestsProgress"];
+            NodeSerializationSettings SerializationSettings = new();
+
+            foreach (var node in progressNodes)
+            {
+                var questStatus = node.Children["MapValue"][0].Children["Quest"][0];
+                var attributes = questStatus.Attributes;
+
+                var objectiveId = attributes["ObjectiveID"].Value.ToString();
+                var stepId = attributes["UnlockedByStepID"].Value.ToString();
+                var isUnlocked = (bool)attributes["QuestUnlocked"].Value;
+                var isDisabled = (bool)attributes["QuestDisabled"].Value;
+
+                // get quest flags
+
+                // flag category
+                var prefix = objectiveId.Contains("_")
+                    ? objectiveId.Split('_')[0]
+                    : "OTHER";
+
+                Quests.Add(new QuestItemViewModel
+                {
+                    ObjectiveId = objectiveId,
+                    StepId = stepId,
+                    IsUnlocked = isUnlocked,
+                    IsDisabled = isDisabled,
+                    Prefix= prefix
+                });
+
+                System.Diagnostics.Debug.WriteLine($"NODE: {node.Name}\nobjective: {objectiveId}\nstepID: {stepId}\nunlocked?: {isUnlocked}\ndisabled?: {isDisabled}");
+            }
         }
     }
 }
